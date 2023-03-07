f96b058ad2487c74e3d0e8b75d017886d6f8b9b6
George.Ciuca <123061744+GeorgeCiuca@users.noreply.github.com>
2023-03-07 09:56:21 +0200

    Update price_scraping_app.py
    Added "Update details" option to be able to change product name/ url/ target price

--- price_scraping_app.py
+++ price_scraping_app.py
@@ -10,7 +10,6 @@ import pandas as pd
 from bokeh.plotting import figure, show
 from bokeh.models import HoverTool, DatetimeTickFormatter
 from bokeh.io import curdoc
-import bokeh
 
 
 def scrapping_data(product, url, best_price):
@@ -105,10 +104,13 @@ class App():
         self.statistics_btn = Button(self.window, text="      Statistics    ", bg="#7C8363", fg="#EDF4F2",
                                      font=("Calibri", 14))
         self.statistics_btn.bind("<ButtonRelease-1>", self.statistics_page)
+        self.update_btn = Button(self.window, text="Update details",bg="#A36A00", fg="#EDF4F2", font=("Calibri", 14))
+        self.update_btn.bind("<ButtonRelease-1>", self.update_page)
         self.check_prices_btn.place(x=500, y=60)
         self.see_products_btn.place(x=650, y=60)
         self.add_product_btn.place(x=800, y=60)
-        self.statistics_btn.place(x=800, y=120)
+        self.update_btn.place(x=800, y=110)
+        self.statistics_btn.place(x=800, y=160)
         self.delete_product_btn.place(x=950, y=60)
         self.exit_btn.place(x=1110, y=60)
         self.image_frame = Label(self.window, image=app_logo, bg="#31473A")
@@ -240,7 +242,7 @@ class App():
         for item in self.file_products:
             try:
                 array.append(scrapping_data(item[0], item[1], item[2]))
-                sleep(randint(10, 20))
+                sleep(randint(11, 20))
             except AttributeError:
                 messagebox.showinfo("Error", "Capcha error")
         for x in range(len(array)):
@@ -263,9 +265,9 @@ class App():
         self.read_intro.place(x=40, y=60)
         self.clicked = StringVar()
         self.clicked.set("Select product...")
-        self.drop = OptionMenu(self.add_frame, self.clicked, *product_names)
-        self.drop.config(font=("Calibri", 15))
-        self.drop.place(x=40, y=150)
+        drop = OptionMenu(self.add_frame, self.clicked, *product_names)
+        drop.config(font=("Calibri", 15))
+        drop.place(x=40, y=150)
         self.get_info_button = Button(self.add_frame, text="--> Get info", font=("Calibri", 15), bg="#A36A00",
                                       fg="#EDF4F2")
         self.get_info_button.bind("<ButtonRelease-1>", self.additional_info)
@@ -274,7 +276,6 @@ class App():
     def additional_info(self, event):
         self.choice = self.clicked.get()
         self.get_info_button.destroy()
-        self.drop.configure(state="disabled")
         try:
             myDB = mysql.connect(host="localhost", user="root", passwd="mysqlPassword@", database="emag")
             mycursor = myDB.cursor(buffered=True)
@@ -332,7 +333,7 @@ class App():
         curdoc().theme = "dark_minimal"
         tooltips = HoverTool(tooltips=[
             ("index", "$index"),
-            ("Date", "$x{%F}"), ("Price", "@Price{0.00}"),
+            ("Date", "$x{%F}"), ("Price", "@Price"),
         ], formatters={"$x": "datetime"})
         p = figure(title="Historical data", x_axis_label="Date", x_axis_type="datetime", y_axis_label="Price",
                    tools=[tooltips], width=self.width-50,
@@ -343,6 +344,86 @@ class App():
         show(p)
         mycursor.close()
         myDB.close()
+
+
+    def update_page(self, event):
+        self.add_frame = Frame(self.window, bg="#31473A")
+        self.add_frame.place(x=0, y=0, height=self.height, width=self.width)
+        self.update_button = Button(self.add_frame, text="Update details", bg="#DAA7A2", fg="white",
+                                    font=("Calibri", 15))
+        self.update_button.bind("<ButtonRelease-1>", self.update_func)
+        self.update_button.place(x=170, y=500)
+        self.back_button = Button(self.add_frame, text="<--Back to main menu", font=("Calibri", 15),
+                                  command=lambda: self.add_frame.destroy(), bg="#7C8363", fg="#EDF4F2")
+        self.back_button.place(x=800, y=60)
+        self.file_products = get_products_from_db()
+        product_names = [item[0] for item in self.file_products]
+        self.read_intro = Label(self.add_frame, text="Current products in scope:",
+                                fg="white", bg="#31473A",
+                                font=("Calibri", 15, "bold"))
+        self.read_intro.place(x=40, y=80)
+        self.clicked = StringVar()
+        self.clicked.set("Select product...")
+        drop = OptionMenu(self.add_frame, self.clicked, *product_names)
+        drop.config(font=("Calibri", 15))
+        drop.place(x=40, y=140)
+        self.fetch_button = Button(self.add_frame, text="Fetch data", bg="#A36A00", fg="#EDF4F2",
+                                         font=("Calibri", 15))
+        self.fetch_button.bind("<ButtonRelease-1>", self.option_menu_answer)
+        self.fetch_button.place(x=800, y=110)
+
+
+    def update_func(self, event):
+        _product_name = self.product_name_box.get()
+        _product_url = self.product_url_box.get()
+        _target_price = self.target_price_box.get()
+        try:
+            myDB = mysql.connect(host="localhost", user="root", passwd="mysqlPassword@", database="emag")
+            mycursor = myDB.cursor(buffered=True)
+            sql = f"UPDATE inputs SET product_name = '{_product_name}', product_url= '{_product_url}', target_price = '{_target_price}' WHERE ID = '{self.id}'"
+            mycursor.execute(sql)
+            myDB.commit()
+            messagebox.showinfo("Success", "Details updated")
+            self.product_name_box.delete(0, END)
+            self.product_url_box.delete(0, END)
+            self.target_price_box.delete(0, END)
+            mycursor.close()
+            myDB.close()
+        except:
+            messagebox.showinfo("Error", "Please contact developer")
+
+    def option_menu_answer(self, event):
+        choice = self.clicked.get()
+        try:
+            myDB = mysql.connect(host="localhost", user="root", passwd="mysqlPassword@", database="emag")
+            mycursor = myDB.cursor(buffered=True)
+            sql = f"SELECT *  FROM inputs WHERE product_name = '{choice}'"
+            mycursor.execute(sql)
+            myresult=mycursor.fetchall()
+            self.product_name = Label(self.add_frame, text="Product name:", font=("Serif", 15))
+            self.product_name_box = Entry(self.add_frame, width=50, font=("Serif", 15))
+            self.product_url = Label(self.add_frame, text="Product URL: ", font=("Serif", 15))
+            self.product_url_box = Entry(self.add_frame, width=50, font=("Serif", 15))
+            self.target_price = Label(self.add_frame, text=" Target price:", font=("Serif", 15))
+            self.target_price_box = Entry(self.add_frame, width=50, font=("Serif", 15))
+            self.product_name.place(x=40, y=200)
+            self.product_name_box.place(x=350, y=200)
+            self.product_url.place(x=40, y=250)
+            self.product_url_box.place(x=350, y=250)
+            self.target_price.place(x=40, y=300)
+            self.target_price_box.place(x=350, y=300)
+            for item in myresult:
+                self.id = item[0]
+                self.product_name_box.delete(0,END)
+                self.product_name_box.insert(0, f"{item[1]}")
+                self.product_url_box.delete(0,END)
+                self.product_url_box.insert(0, f"{item[2]}")
+                self.target_price_box.delete(0,END)
+                self.target_price_box.insert(0, f"{item[3]}")
+            mycursor.close()
+            myDB.close()
+        except:
+            messagebox.showinfo("Error", "Please contact developer")
 
 if __name__ == '__main__':
     app = App()
